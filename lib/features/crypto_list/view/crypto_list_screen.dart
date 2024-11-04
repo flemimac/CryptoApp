31231557d092e0328e1df64e00f62fc7edc3eda1
flemimac <alex.miazim.2006@gmail.com>
2024-11-04 16:57:02 +0200

    add pattern Bloc

--- lib/features/crypto_list/view/crypto_list_screen.dart
+++ lib/features/crypto_list/view/crypto_list_screen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_learn/features/crypto_list/bloc/crypto_list_bloc.dart';
 import 'package:flutter_learn/repositories/crypto_coins/crypto_coins.dart';
 import 'package:get_it/get_it.dart';
 import '../widgets/widgets.dart';
@@ -15,36 +17,73 @@ class CryptoListScreen extends StatefulWidget {
 }
 
 class _CryptoListScreen extends State<CryptoListScreen> {
-  List<CryptoCoin>? _cryptoCoinsList;
+  final _cryptoListBloc = CryptoListBloc(GetIt.I<AbstractCoinsRepository>());
 
   @override
   void initState() {
-    _loadCryptoCoins();
+    _cryptoListBloc.add(LoadCryptoList());
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Crypto Currenciest List'),
-      ),
-      body: (_cryptoCoinsList == null)
-          ? Center(child: const CircularProgressIndicator())
-          : ListView.separated(
-              padding: EdgeInsets.only(top: 16),
-              itemCount: _cryptoCoinsList!.length,
-              separatorBuilder: (context, index) => const Divider(),
-              itemBuilder: (context, i) {
-                final coin = _cryptoCoinsList![i];
-                return CryptoCoinTile(coin: coin);
-              },
-            ),
-    );
-  }
+        appBar: AppBar(
+          title: const Text('Crypto Currenciest List'),
+        ),
+        body: BlocBuilder<CryptoListBloc, CryptoListState>(
+          bloc: _cryptoListBloc,
+          builder: (context, state) {
+            if (state is CryptoListLoaded) {
+              return ListView.separated(
+                padding: EdgeInsets.only(top: 16),
+                itemCount: state.coinsList.length,
+                separatorBuilder: (context, index) => const Divider(),
+                itemBuilder: (context, i) {
+                  final coin = state.coinsList[i];
+                  return CryptoCoinTile(coin: coin);
+                },
+              );
+            }
+            if (state is CryptoListLoadingFailure) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Something went wrong',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 24),
+                    ),
+                    Text(
+                      'Please try againg later',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18),
+                    ),
+                  ],
+                ),
+              );
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        )
 
-  Future<void> _loadCryptoCoins() async {
-    _cryptoCoinsList = await GetIt.I<AbstractCoinsRepository>().getCoinsList();
-    setState(() {});
+        // (_cryptoCoinsList == null)
+        //     ? Center(child: const CircularProgressIndicator())
+        //     : ListView.separated(
+        //         padding: EdgeInsets.only(top: 16),
+        //         itemCount: _cryptoCoinsList!.length,
+        //         separatorBuilder: (context, index) => const Divider(),
+        //         itemBuilder: (context, i) {
+        //           final coin = _cryptoCoinsList![i];
+        //           return CryptoCoinTile(coin: coin);
+        //         },
+        //       ),
+        );
   }
 }
